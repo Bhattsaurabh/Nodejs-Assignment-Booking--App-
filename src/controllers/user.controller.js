675e2c2f03af51b17793e9bf35474b6f574772a9
saurabhbhatt <bhattsourav4@gmail.com>
2025-05-09 23:42:14 +0530

    add controllers and routes.

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,215 @@
+import  {asyncHandler} from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
+
+
+
+const generateAccessandRefreshTokens = async (userId) => {
+
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken                    // assigning refreshtoken to user
+        await user.save({ validateBeforeSave: false })        // saving refereshtoken in user
+
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating refresh and access token")
+    }
+}
+
+const registerUser  =   asyncHandler( async (req, res) =>{
+    // get user details from frontend
+    // validation   -   not empty
+    // check if user is already exists: username, email
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation 
+    // return response
+
+
+
+    // extract data from req.body (sirf text data hold krta h)
+    console.log(req.body);
+    const { name, email, phone, password } = req.body          //destructuring
+    console.log("fullName: ", name, "email: ", email, "Phone: ", phone)
+
+    // check if any field is empty or not
+    if (
+        [ name, email, phone, password].some((field) =>
+            field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All field are required")
+    }
+
+    //checking email validation
+    if (req.body.email.indexOf('@') == -1) {
+        throw new ApiError(400, "Email is not valid.")
+    }
+
+    //checking user exists or not
+    const existedUser = await User.findOne({
+        $or: [{ name }, { email }]          //  means checking all field using OR operation
+    })
+
+    if (existedUser) {                          // if user already registered then throw error
+        throw new ApiError(409, "User with email or name already exists")
+    }
+
+    console.log("user created")
+
+
+    // create entry in db
+    // save hone  se  pehle pre method chlega
+
+    const user = await User.create({
+        name: name.toLowerCase(),
+        email,
+        phone,
+        password
+    })
+
+
+    // remove password and refresh token field from response
+    const createdUser = await User.findById(user.id).select(         // mongoDB auto generate a id for every entry 
+        "-password -refreshToken"
+    )
+
+    // check for user creation
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering user")
+    }
+
+
+    console.log("User Registered Successfully")
+    //returning  response to user interface
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User Registered Successfully")
+    )
+
+
+})
+
+
+
+const loginUser = asyncHandler(async (req, res) => {
+
+    // req body -> data
+    // email
+    // find the user
+    // verify user is there or not
+    // check password
+    // access and refresh token
+    // send cookie
+
+    const { email, password } = req.body
+
+    //checking  email is there or not
+    if (!(email)) {
+        throw new ApiError(400, "email is required")
+    }
+
+    // find user has a valid email by checking in mongoDB
+    const user = await User.findOne(email)
+
+    if (!user) {
+        throw new ApiError(404, "user does not exists")
+    }
+
+    // check user input password is correct or not
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid Password")
+    }
+
+    // getting accesstoken and refreshtoken
+    // accesstoken => user ko login krte time accesstoken provide kiya jata h for a fixed time.
+    // refreshtoken => accesstoken expire hone k baad user ko bina dubara login kraye refreshtoken se user ko login rkha jata h agr
+    // user ka refreshtoken DB k refreshtoken se match hogya.
+    const { accessToken, refreshToken } = await generateAccessandRefreshTokens(user._id)
+
+    //sending cookie
+    const loggedInUser = await User.findById(user.id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,         // cookie only manage by server
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(200, {
+                user: loggedInUser, accessToken, refreshToken
+            },
+                "User logged in Successfully"
+            )
+        )
+})
+
+// logout user
+// logout karne k liye user ko jo login pr tokens diye wo sb clear karne honge.
+// user ko logout karne k liye user ka access lena pdega tabhi toh accesstoken and refreshtoken hatayenge
+// issliye ye middleware ka use krk logout method par jane se pehle req mai req.user ko add krdenge jisme  access and refresh tokens honge.
+
+
+
+// jab user ka accesstoken expire hojyga then usko dubara login na krne k liye uska refreshtoken verify krenge
+// login pe user ko jo refreshtoken diya h usko DB mai save wale se verify krenge.
+// user ko diya hua koi b token encoded hota h or DB mai decoded m hota h
+const refreshAccessToken = asyncHandler(async (req, res) => {
+
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+    if (!incomingRefreshToken) {
+        throw new ApiError(401, "Unauthorized Request")
+    }
+
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+
+        const user = await User.findById(decodedToken?._id)
+
+        if (!user) {
+            throw new ApiError(401, "Invalid refresh token")
+        }
+
+        if (incomingRefreshToken !== user?.refreshToken) {
+            throw new ApiError(401, "Refresh token is expired or used")
+        }
+
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+
+        const { accessToken, newrefreshToken } = await generateAccessandRefreshTokens(user._id)
+
+        return res
+            .status(200)
+            .cookie("accessToken", accessToken, options)
+            .cookie("refreshToken", newrefreshToken, options)
+            .json(
+                new ApiResponse(200, { accessToken, newrefreshToken },
+                    "Access token refreshed"
+                )
+            )
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid refresh token")
+    }
+
+
+})
+
+
+
+
+export {registerUser}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 
 const userSchema = new mongoose.Schema(
@@ -37,6 +38,63 @@ const userSchema = new mongoose.Schema(
 )
 
 
+
+
+//you can write methods directly into Schema
+
+//making hash of your password
+// only change if password modify
+// yaha pe callback ki jagah "function" ka use issliye hua taki userSchema k field ka b access le ske "this" keyword se
+// pre method kuch bhi "save" krne se pehle run hojyga
+// pre method k pass purane or new password ka access hoga tabhi toh check kr payega {this.ismodify("password")}
+// DB mai save hone se pehle ye run hoga tabhi toh password  hash m change hora h DB mai.
+
+userSchema.pre('save', async function (next)  {
+    // pre function update password
+    if(!this.isModified('password'))    
+        return next();
+    
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+
+// check user input password
+// user input password or hashed password ko compare krenge
+// methods k pass sare data ka access hoga jese isme hashed password ka h (this.password)
+// this se database ka data access kr skte h kisi methods k andar
+userSchema.methods.isPasswordCorrect = async function (userInputpassword) {
+
+   return await bcrypt.compare(userInputpassword, this.password)      ///it returns a boolean value
+}
+
+//json web token is a type of key.
+userSchema.methods.generateAccessToken = function(){
+  return  jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return  jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 
 
